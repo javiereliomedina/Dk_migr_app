3e7d98b7aa7ed2b1b7c9aa2da236a0cc0f34735e
Javier Elio <javiereliomedina@gmail.com>
2021-09-30 14:50:06 +0200

    Problems with new page and shinyapp.io - delte new lines and it works!!

--- app.R
+++ app.R
@@ -1,8 +1,61 @@
-# Get data
 
-source("packages.R")
-source("data.R")
-source("local_adm_units.R")
+library(shiny)
+library(danstat) 
+library(tidyverse)
+library(readr)
+library(dint)
+library(patchwork)
+library(gridExtra)
+library(forcats)
+library(RColorBrewer)
+library(furrr)
+library(sf)
+library(giscoR)
+library(viridis)
+library(gganimate)
+library(tidytable)
+
+
+# # if(!require("devtools"))  install.packages("devtools")
+# if(!require("ggsflabel")) devtools::install_github("yutannihilation/ggsflabel")
+library(ggsflabel)
+
+# Data ----
+
+## Local Administrative Units (LAU) ----
+dk_lau <- readRDS(file = "DAGI/KOMMUNE.rds") %>% 
+  mutate(KOMNAVN = factor(KOMNAVN)) %>% 
+  st_transform(crs = 4326) %>% 
+  group_by(KOMNAVN) %>%
+  summarise(geometry = st_union(geometry)) %>% 
+  ungroup() %>% 
+  rename(LAU_NAME = KOMNAVN)
+
+## Immigrants and their descendant in Denmark (Table: FOLK1C) ----
+
+id_table <- "FOLK1C"
+var_pop <- get_table_metadata(table_id = id_table, variables_only = TRUE)
+
+countries <- as_tibble(var_pop$values[[5]]) %>% 
+  mutate(text = factor(text))
+
+dates <- var_pop$values[[6]]$id %>%
+  gsub("K", "", .) %>% 
+  as.integer() %>% 
+  as_date_yq() %>% 
+  first_of_quarter()
+
+municipalities <- var_pop$values[[1]] %>% 
+  as_tibble() %>% 
+  mutate(id = as.numeric(id)) %>% 
+  filter(id > 100)
+
+## Palette YlGnBu, with 9 colours
+my_pal <- brewer.pal(9, "YlGnBu") 
+## Add more colours to this palette :
+my_pal <- rev(colorRampPalette(my_pal)(10))
+
+cumsum__breaks <- seq(0, 100, 10)
 
 big_cities <- c("København", "Aarhus", "Odense", "Aalborg")
 big_cities <- filter(dk_lau, LAU_NAME %in% big_cities) 
--- local_adm_units.R
+++
@@ -1,8 +0,0 @@
-# Local Administrative Units (LAU)
-dk_lau <- readRDS(file = "DAGI/KOMMUNE.rds") %>% 
-  mutate(KOMNAVN = factor(KOMNAVN)) %>% 
-  st_transform(crs = 4326) %>% 
-  group_by(KOMNAVN) %>%
-  summarise(geometry = st_union(geometry)) %>% 
-  ungroup() %>% 
-  rename(LAU_NAME = KOMNAVN)
